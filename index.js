97b71c401c2c7f8e9242d10c931fdefc9dd99d7c
praWeb <prasanna.gaddam@shiftcommerce.com>
2019-03-08 13:12:32 +0000

    add checks for test link being running

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const request = require("request")
 const WebpageTest = require('./lib/webpage/index')
 const Slack = require('./lib/slack/index')
 const DataParser = require('./lib/parse_data')
@@ -13,28 +14,32 @@ module.exports = app => {
   app.log('Yay, the app was loaded!')
 
   app.on(`*`, async context => {
-    if (context.name === 'deployment_status' && context.payload.deployment_status.state !== 'success'){
-      const issueComment = context.issue({ number: parseIssueId(test_url), body: 'Sorry deployment has been failed' })
+    //@TODO This command should be changed in case of pull request
+    const testUrl = process.env.WEBPAGETEST_TEST_URL || `https://${context.payload.deployment_status.environment}.herokuapp.com`
+
+    if (context.name === 'deployment_status' && context.payload.deployment_status.state !== 'success') {
+      const issueComment = context.issue({ number: parseIssueId(testUrl), body: 'Sorry deployment has been failed' })
       return context.github.issues.createComment(issueComment)
     }
-
-    const test_url = process.env.WEBPAGETEST_TEST_URL || `https://${context.payload.deployment_status.environment}.herokuapp.com`
-
-    // @TODO Ensure link is up and running before starting test
-
-    webpagetest.run(test_url, function (message) {
-      acknowledge(context, parseIssueId(test_url), message)
-    }).then((testresults) => {
-      // @TODO: Have to parse the data to print in required format
-      // @TODO: Handle error rejections too
-      if (testresults) {
-        const message = dataParser.parse(testresults)
-        acknowledge(context, parseIssueId(test_url), message)
-      }
+    checkUrlStatus(testUrl).then(function () {
+      webpagetest.run(testUrl, function (message) {
+        acknowledge(context, parseIssueId(testUrl), message)
+      }).then((testresults) => {
+        if (testresults) {
+          const message = dataParser.parse(testresults)
+          acknowledge(context, parseIssueId(testUrl), message)
+        }
+      }).error((error) => {
+        // Not sure if this works
+        acknowledge(context, parseIssueId(testUrl), error)
+      })
+    },function (error) {
+      return acknowledge(context, parseIssueId(testUrl), `Test URL ${testUrl} is not working.`)
     })
   })
 
   const parseIssueId = (url) => {
+    console.log(url)
     return url.match(/pr-([\d]+)/)[1]
   }
 
@@ -42,5 +47,23 @@ module.exports = app => {
     const issueComment = context.issue({ number: issueId, body: body })
     slackClient.postMessage(body)
     return context.github.issues.createComment(issueComment)
+  }
+
+  const checkUrlStatus = (url) => {
+    return new Promise((resolve, reject) => {
+      request({
+        uri: url,
+        method: 'GET',
+        timeout: 10000
+      }, function (error, response, body) {
+        if (error) {
+          return reject(error)
+        } else if (response.statusCode < 200 || response.statusCode > 400) {
+          return reject(error)
+        } else {
+          return resolve(true)
+        }
+      })
+    })
   }
 }
