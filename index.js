e93ca75301bb826a92e5502a7ee8372280c64f5b
praWeb <prasanna.gaddam@shiftcommerce.com>
2019-05-17 11:18:08 +0100

    Refactor complete logic and seperate individual modules

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-'use strict';
+'use strict'
 
-const request = require("request")
-const WebpageTest = require('./lib/webpage/index')
-const Slack = require('./lib/slack/index')
-const DataParser = require('./lib/parse_data')
-const PageSpeed = require('./lib/webpage/page_speed')
-
-const webpagetest = new WebpageTest()
-const slackClient = new Slack()
-const dataParser = new DataParser()
-const pageSpeedScore = new PageSpeed()
+const initTest = require('./lib/init_test')
 
 module.exports = app => {
   // Just for monitor purpose
@@ -17,71 +8,6 @@ module.exports = app => {
 
   // Start receiving events
   app.on(`*`, async context => {
-    // Read app configuration
-    const config = await context.config('webpack_config.yml')
-
-    //@TODO This command should be changed in case of pull request
-    let testUrl = ''
-
-    if (context.name === 'deployment_status') {
-      testUrl = process.env.WEBPAGETEST_TEST_URL || `https://${context.payload.deployment_status.environment}.herokuapp.com`
-    } else {
-      // We handle only the scenario of deployments
-      return ''
-    }
-
-    if (context.name === 'deployment_status' && context.payload.deployment_status.state !== 'success') {
-      const issueComment = context.issue({ number: parseIssueId(testUrl), body: 'Sorry deployment has been failed' })
-      return context.github.issues.createComment(issueComment)
-    }
-
-    checkUrlStatus(testUrl)
-    .then(() => {
-      webpagetest.run(testUrl, (message) => {
-        acknowledge(context, parseIssueId(testUrl), message)
-      }).then((testresults) => {
-        console.log(testresults.data.average)
-        if (testresults) {
-          pageSpeedScore.getScore(testUrl).then(function (data) {
-            console.log(data.ruleGroups)
-            console.log(data.formattedResults.ruleResults)
-            acknowledge(context, parseIssueId(testUrl), dataParser.parse(testresults.data, data, config))
-          })
-        }
-      }, (error) => {
-        // Not sure if this works
-        acknowledge(context, parseIssueId(testUrl), error)
-      })
-    }, (error) => {
-      return acknowledge(context, parseIssueId(testUrl), `Test URL ${testUrl} is not working.`)
-    })
+    initTest.trigger(context)
   })
-
-  const parseIssueId = (url) => {
-    return url.match(/pr-([\d]+)/)[1]
-  }
-
-  const acknowledge = (context, issueId, body) => {
-    const issueComment = context.issue({ number: issueId, body: body })
-    slackClient.postMessage(body)
-    return context.github.issues.createComment(issueComment)
-  }
-
-  const checkUrlStatus = (url) => {
-    return new Promise((resolve, reject) => {
-      request({
-        uri: url,
-        method: 'GET',
-        timeout: 10000
-      }, function (error, response, body) {
-        if (error) {
-          return reject(error)
-        } else if (response.statusCode < 200 || response.statusCode > 400) {
-          return reject(error)
-        } else {
-          return resolve(true)
-        }
-      })
-    })
-  }
 }
